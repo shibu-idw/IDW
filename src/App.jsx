075e211b7c5bb83c0280923861562b96c9bce296
sathish6502 <mahamoodbasha.idw@gmail.com>
2026-04-24 15:25:37 +0530

    Termsand privacy policy written

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Terms from './Components/Terms/Terms'
+import PrivatePolicy from './Components/PrivatePolicy/PrivatePolicy'
 
 const App = () => {
   return (
     <div>
-      
+      <Terms/>
+      {/* <PrivatePolicy/> */}
     </div>
   )
 }
