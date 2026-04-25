2d5ab51383b11403790fc89373df884726037d65
gayathriidw <gayathri.idw@gmail.com>
2026-04-25 16:11:33 +0530

    header file update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+
 import Division from './Pages/Division'
 import Header from "./Components/Header/Header"
 import Home from "./Pages/Home"
-import Services from './Components/Home/Service'
+import About from "./Pages/Aboutpage"
 
 const App = () => {
   return (
-    <Router>
+    <>
       <Header />
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/division" element={<Division />} />
       </Routes>
-
-    </Router>
+    </>
   )
 }
 
