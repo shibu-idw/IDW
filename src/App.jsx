10f75b7dd8d2d9f027699c01b6cd042e2bba2664
rajaduraicbe <rajadurai2496cbe@gmail.com>
2026-04-27 13:58:40 +0530

    1.58 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,26 @@
-<<<<<<< HEAD
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Division from './Pages/Division'
 import Header from "./Components/Header/Header"
 import Home from "./Pages/Home"
-=======
-import Services from "./Components/Services/Services";
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Header from "./Components/Header/Header";
-import Home from "./Pages/Home";
-import About from "./Pages/Aboutpage";
->>>>>>> parent of 49306d7 (Merge remote-tracking branch 'origin/gayu' into Raja)
+import About from "./Pages/Aboutpage"
+import Footer from './Components/Footer/Footer'
+import Contact from "./Components/Contact/contactus"
 
 const App = () => {
   return (
-    <div>
-<<<<<<< HEAD
-      <Header/>
-      <Home/>
-    </div>
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/division" element={<Division />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </>
   )
 }
 
 export default App
-=======
-      <Services />
-      {/* <Header /> */}
-      {/* <Routes> */}
-        {/* <Route path="/" element={<Home />} /> */}
-        {/* <Route path="/about" element={<About />} /> */}
-      {/* </Routes> */}
-    </div>
-  );
-};
-
-export default App;
->>>>>>> parent of 49306d7 (Merge remote-tracking branch 'origin/gayu' into Raja)
