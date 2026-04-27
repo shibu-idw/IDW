909ca52bc331d6f77aeea5c8371a5d678afff3c2
shibu-idw <shibu.idw@gmail.com>
2026-04-27 12:57:31 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React from 'react'
 import Header from "./Components/Header/Header"
 import Home from "./Pages/Home"
 =======
+=======
+>>>>>>> parent of e8df608 (Merge remote-tracking branch 'origin/gayu' into shibu)
 import Services from "./Components/Services/Services";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Home from "./Pages/Home";
 import About from "./Pages/Aboutpage";
+<<<<<<< HEAD
+>>>>>>> parent of e8df608 (Merge remote-tracking branch 'origin/gayu' into shibu)
+=======
 >>>>>>> parent of e8df608 (Merge remote-tracking branch 'origin/gayu' into shibu)
 
 const App = () => {
   return (
     <div>
+<<<<<<< HEAD
 <<<<<<< HEAD
       <Header/>
       <Home/>
@@ -23,6 +30,8 @@ const App = () => {
 
 export default App
 =======
+=======
+>>>>>>> parent of e8df608 (Merge remote-tracking branch 'origin/gayu' into shibu)
       <Services />
       {/* <Header /> */}
       {/* <Routes> */}
@@ -34,4 +43,7 @@ export default App
 };
 
 export default App;
+<<<<<<< HEAD
+>>>>>>> parent of e8df608 (Merge remote-tracking branch 'origin/gayu' into shibu)
+=======
 >>>>>>> parent of e8df608 (Merge remote-tracking branch 'origin/gayu' into shibu)
