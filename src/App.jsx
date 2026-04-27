cd03f927ca3b057cbd13c2f0daa70c741b94579f
shibu-idw <shibu.idw@gmail.com>
2026-04-27 12:56:53 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+<<<<<<< HEAD
 import React from 'react'
 import Header from "./Components/Header/Header"
 import Home from "./Pages/Home"
+=======
+import Services from "./Components/Services/Services";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Home from "./Pages/Home";
+import About from "./Pages/Aboutpage";
+>>>>>>> parent of e8df608 (Merge remote-tracking branch 'origin/gayu' into shibu)
 
 const App = () => {
   return (
     <div>
+<<<<<<< HEAD
       <Header/>
       <Home/>
     </div>
@@ -12,3 +22,16 @@ const App = () => {
 }
 
 export default App
+=======
+      <Services />
+      {/* <Header /> */}
+      {/* <Routes> */}
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* <Route path="/about" element={<About />} /> */}
+      {/* </Routes> */}
+    </div>
+  );
+};
+
+export default App;
+>>>>>>> parent of e8df608 (Merge remote-tracking branch 'origin/gayu' into shibu)
